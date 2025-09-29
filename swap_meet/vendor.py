388a79e1dd8d5c658dc26d653fad778376287774
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-09-29 15:33:11 -0700

    Create Vendor_wave_01 class in vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
+    
+                
+        

9181677c8c3b11e22c8bd71cdfb33707b0e873c2
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-09-29 22:42:16 -0700

    Implement swap_items function in Vendor class and str functions in Item class for wave_03; implement swap_first_item functions
    Co-authored-by: Qiusi_A <SophieQA@users.noreply.github.com>

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -10,4 +10,8 @@ class Item:
 
     
     def get_category(self):
-        return type(self).__name__
+        return "Item"
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,13 +14,32 @@ class Vendor:
         if item in self.inventory:
             self.inventory.remove(item)
             return item
-        return False
+        else:
+            return False
     
     def get_by_id(self, id):
         for item in self.inventory:
             if item.id == id:
                 return item
         return None
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            other_vendor.inventory.append(my_item)
+            self.inventory.remove(my_item)
+            other_vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            return True
+        return False
+
+    def swap_first_item(self, other_vendor):
+        if self.inventory and other_vendor.inventory:
+            self.inventory[0], other_vendor.inventory[0] = other_vendor.inventory[0], self.inventory[0]
+            return True
+        return False
+
+
+
 
 
 

977d6b9622fda84ffdd5812446fdf1cef24f2bb6
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-09-29 22:56:36 -0700

    Implement Clothing class inheriting from Item
    Co-authored-by: Qiusi_A <SophieQA@users.noreply.github.com>

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,13 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+
+class Clothing(Item):
+    def __init__(self, id = None, fabric = "Unknown"):
+        super().__init__(id = id)
+        self.fabric = fabric
+
+    def get_category(Clothing):
+        return "Clothing"
+
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."

f9a4907c0d56fcdc170f540e2d88253019c39c86
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-09-29 23:14:52 -0700

    Implement Electronics class inheriting from Item
    Co-authored-by: Qiusi_A <SophieQA@users.noreply.github.com>

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,12 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, id = None, type = "Unknown"):
+        super().__init__(id = id)
+        self.type = type
+
+    def get_category(self):
+        return "Electronics"
+
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
